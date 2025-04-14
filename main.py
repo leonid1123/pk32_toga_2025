e7dbf81dc4c40da246fbe9b2107ab7efe771c7e7
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2025-04-14 11:08:43 +0300

    окно с заметками

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import toga
 from toga.style.pack import COLUMN, ROW
 from toga.style import Pack
 import sqlite3
+
+
 # сделать приложение "заметки"
 
-
+#https://github.com/leonid1123/pk32_toga_2025
 class Application(toga.App):
     def startup(self):
-        self.main_window = toga.MainWindow()
+        self.notes_list_window = None
+        self.style = Pack(padding=5, flex=1, font_family='Helvetica', font_size=16)
+        self.main_window = toga.MainWindow(size=(600,200))
         self.main_box = toga.Box(style=Pack(direction=COLUMN))
         self.login_box = toga.Box()
         self.login_box.style.update(direction=ROW)
@@ -16,12 +20,12 @@ class Application(toga.App):
         self.btn_box = toga.Box()
         self.btn_box.style.update(direction=COLUMN)
 
-        self.button = toga.Button("Вход в систему", on_press=self.button_handler, style=Pack(padding=5, flex=1))
-        self.login_label = toga.Label("Логин", style=Pack(padding=5, flex=1, font_family='Helvetica', font_size=16))
-        self.pass_label = toga.Label("Пароль", style=Pack(padding=5, flex=1, font_family='Helvetica', font_size=16))
-        self.login_entry = toga.TextInput(style=Pack(padding=5, flex=1))
-        self.pass_entry = toga.TextInput(style=Pack(padding=5, flex=1))
-        self.error_label = toga.Label("Ошибки:", style=Pack(padding=5, flex=1, font_family='Helvetica', font_size=16))
+        self.button = toga.Button("Вход в систему", on_press=self.button_handler, style=self.style)
+        self.login_label = toga.Label("Логин", style=self.style)
+        self.pass_label = toga.Label("Пароль", style=self.style)
+        self.login_entry = toga.TextInput(style=self.style)
+        self.pass_entry = toga.TextInput(style=self.style)
+        self.error_label = toga.Label("Ошибки:", style=self.style)
         self.login_box.add(self.login_label)
         self.login_box.add(self.login_entry)
 
@@ -54,8 +58,7 @@ class Application(toga.App):
         self.cursor.execute(create_table_query)
         self.cnx.commit()
 
-
-        self.cursor.execute("SELECT password FROM users WHERE login=?",(user_login,))
+        self.cursor.execute("SELECT password FROM users WHERE login=?", (user_login,))
         self.cnx.commit()
         ans = self.cursor.fetchall()
         print(ans)
@@ -65,13 +68,44 @@ class Application(toga.App):
             print(pass_tmp)
             if pass_tmp == user_password:
                 print("ok")
+                self.notes_window()
             else:
                 print("не ok")
         #сделать проверку пароля
 
+    def notes_window(self):
+        if self.notes_list_window is None:
+            self.notes_list_window = toga.Window(title="Заметки")
+            notes_window_box = toga.Box()
+            notes_window_box.style.update(direction=COLUMN)
+            self.note1 = toga.Button("Заметка №1", style=self.style, on_press=self.note)
+            self.note2 = toga.Button("Заметка №2", style=self.style)
+            self.note3 = toga.Button("Заметка №3", style=self.style)
+            self.note4 = toga.Button("Заметка №4", style=self.style)
+            self.note5 = toga.Button("Заметка №5", style=self.style)
+            notes_window_box.add(self.note1)
+            notes_window_box.add(self.note2)
+            notes_window_box.add(self.note3)
+            notes_window_box.add(self.note4)
+            notes_window_box.add(self.note5)
 
-app = Application("myApp","myApp")
+            self.notes_list_window.content = notes_window_box
+            self.notes_list_window.show()
+
+    def note(self, widget):
+        #сделать проверку на открытие окна
+        #сделать таблицу с заметками
+        note_win = toga.Window(title="Заметка")
+        note_box = toga.Box()
+        note_box.style.update(direction=COLUMN)
+        note_view = toga.MultilineTextInput(style=self.style)
+        note_btn = toga.Button("OK", style=self.style, on_press=note_win.close)
+        note_box.add(note_view)
+        note_box.add(note_btn)
+        note_win.content = note_box
+        note_win.show()
+
+
+
+app = Application("myApp", "myApp")
 app.main_loop()
-
-
-#https://ctxt.io/2/AAB4Kx_ZEQ
